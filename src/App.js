9470c346c33e36bb3d4eb9b3899c081fad7dea90
Naveen Kumar <naveenkumarrockz362@gmail.com>
2024-09-14 20:01:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import EventPage from './pages/EventPage';
+import CreateEventPage from './pages/CreateEventPage';
+import Header from './components/Header';
+import EventDetailPage from './pages/EventDetailPage';
+import ManageJoinRequestsPage from './pages/ManageJoinRequestsPage';
+import { UserProvider } from './context/userContext';
+import ProtectedRoute from './components/ProtectedRoute';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <UserProvider>
+        <Routes>
+          <Route path="/"  element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={ <RegisterPage />} />
+          <Route path="/events" element={<ProtectedRoute>
+                <EventPage />
+                </ProtectedRoute>
+                
+             } />
+          <Route path="/create-event" element={ 
+            <ProtectedRoute>
+                <CreateEventPage />
+                </ProtectedRoute>
+              } />
+          <Route path="/events/:id" element={
+            <ProtectedRoute>
+                <EventDetailPage />
+                </ProtectedRoute>}
+               />
+          <Route path="/events/:eventId/manage-requests" element={
+            <ProtectedRoute>
+                <ManageJoinRequestsPage />
+                </ProtectedRoute>
+              } />
+        </Routes>
+        </UserProvider>
+      </div>
+    </Router>
   );
 }
 
